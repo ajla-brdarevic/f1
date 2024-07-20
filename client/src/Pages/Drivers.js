65ce89b3c800c15ff2c326434e491d9262363298
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2024-07-20 18:09:55 +0200

    design for drivers

--- client/src/Pages/Drivers.js
+++ client/src/Pages/Drivers.js
@@ -8,7 +8,7 @@ function Drivers() {
         fetch('http://localhost:5000/api/drivers')
             .then(response => response.json())
             .then(data => {
-                console.log(data); // Proverite podatke ovde
+                console.log(data); 
                 setDrivers(data);
             })
             .catch(error => console.error('Error fetching data:', error));
@@ -18,12 +18,11 @@ function Drivers() {
     return (
         <div>
             <h1>Formula 1 Drivers</h1>
-            <table>
+            <table className='driversTable'>
                 <thead>
                     <tr>
                         <th></th>
-                        <th>Given Name</th>
-                        <th>Family Name</th>
+                        <th>Driver</th>
                         <th>Date of Birth</th>
                         <th>Nationality</th>
                     </tr>
@@ -32,8 +31,9 @@ function Drivers() {
                     {drivers.map(driver => (
                         <tr key={driver.driverId}>
                             <td>{driver.code}</td>
-                            <td>{driver.givenName}</td>
-                            <td>{driver.familyName}</td>
+                            <a href={driver.url} target="_blank" rel="noopener noreferrer">
+                                {driver.givenName} {driver.familyName}
+                            </a>
                             <td>{driver.dateOfBirth}</td>
                             <td>{driver.nationality}</td>
                         </tr>

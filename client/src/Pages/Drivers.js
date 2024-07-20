b96d8db2944651aca387b50f1136086d303a425d
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2024-07-20 17:55:42 +0200

    update for drivers Microsoft radi pa je sad okej

--- client/src/Pages/Drivers.js
+++ client/src/Pages/Drivers.js
@@ -1,23 +1,45 @@
 import React, { useState, useEffect } from 'react';
+import './Drivers.css';
 
 function Drivers() {
     const [drivers, setDrivers] = useState([]);
 
     useEffect(() => {
-        fetch('/api/drivers')
+        fetch('http://localhost:5000/api/drivers')
             .then(response => response.json())
-            .then(data => setDrivers(data))
+            .then(data => {
+                console.log(data); // Proverite podatke ovde
+                setDrivers(data);
+            })
             .catch(error => console.error('Error fetching data:', error));
     }, []);
+
 
     return (
         <div>
             <h1>Formula 1 Drivers</h1>
-            <ul>
-                {drivers.map(driver => (
-                    <li key={driver.driverId}>{driver.givenName} {driver.familyName}</li>
-                ))}
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Given Name</th>
+                        <th>Family Name</th>
+                        <th>Date of Birth</th>
+                        <th>Nationality</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {drivers.map(driver => (
+                        <tr key={driver.driverId}>
+                            <td>{driver.code}</td>
+                            <td>{driver.givenName}</td>
+                            <td>{driver.familyName}</td>
+                            <td>{driver.dateOfBirth}</td>
+                            <td>{driver.nationality}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
